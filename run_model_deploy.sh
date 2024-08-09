bb2e791cb9ba18df5c1d82f1fce85736b94a0067
AndreMenezesDS <andalves994@gmail.com>
2024-08-09 11:48:45 -0300

    Updated 6.2: Run Model Deploy e notebook fix

--- run_model_deploy.sh
+++ run_model_deploy.sh
@@ -2,7 +2,7 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #paths
-path_to_root='/home/ubuntu/repos/PA005_clustering_fidelity_program'
+path_to_root='/home/ubuntu/PA005_clustering_fidelity_program'
 path_to_env='/home/ubuntu/.pyenv/versions/pa005/bin'
 
-$path_to_env/papermill $path_to_root/src/models/pa005_05_deploy_local.ipynb $path_to_root/reports/pa005_05_deploy_local$data.ipynb
+$path_to_env/papermill $path_to_root/src/models/pa005_05_deploy_deploy.ipynb $path_to_root/reports/pa005_05_deploy_deploy$data.ipynb
