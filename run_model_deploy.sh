ae8cb096625a5b6b9fe680e853a41c48487a7199
AndreMenezesDS <andalves994@gmail.com>
2024-08-23 22:07:44 -0300

    Updated 6.7: Fixed Deploy Reports

--- run_model_deploy.sh
+++ run_model_deploy.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path_to_root='/home/ubuntu/PA005_clustering_fidelity_program'
 path_to_env='/home/ubuntu/.pyenv/versions/pa005/bin'
 
-$path_to_env/papermill $path_to_root/src/models/pa005_05_deploy_cloud.ipynb $path_to_root/reports/pa005_05_deploy_cloud$data.ipynb
+$path_to_env/papermill $path_to_root/src/models/pa005_05_deploy_cloud_2.ipynb $path_to_root/reports/pa005_05_deploy_cloud$data.ipynb
